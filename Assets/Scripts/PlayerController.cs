dc7323479db7d1ed8f17806836fd8aa47c267efc
smk-13 <simonmaximiliankramer13@gmail.com>
2025-01-17 15:56:29 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float turnSpeed = 3;
+
+    float moveAxis;
+    float turnAxis;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveAxis = value.Get<float>() * moveSpeed;
+    }
+
+    void OnTurn(InputValue value)
+    {
+        turnAxis = value.Get<float>() * turnSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.up * moveAxis);
+        rb.AddTorque(turnAxis);
+    }
+
+    private void OnDisable()
+    {
+        moveAxis = 0;
+        turnAxis = 0;
+        rb.linearVelocity = Vector2.zero;
+    }
+}
