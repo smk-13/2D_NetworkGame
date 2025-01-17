57807616d604a7b863167feecb78558af44061fb
smk-13 <simonmaximiliankramer13@gmail.com>
2025-01-17 17:04:53 +0100

    added network color

---
+++ Assets/Scripts/PlayerNetworkColor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerNetworkColor : NetworkBehaviour
+{
+    [SerializeField] Color playerOneColor = new Color(1, 0, 0, 0.5f);
+    [SerializeField] Color playerTwoColor = new Color(0, 0, 1, 0.5f);
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.color = (OwnerClientId % 2 == 0) ? playerOneColor : playerTwoColor;
+    }
+
+}
